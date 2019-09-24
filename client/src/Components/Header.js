82f563357a229fc8b700fd218c3718c72b80db1f
Simone Beaver <simone.beaver@verizon.com>
2019-09-24 08:10:30 -0500

    header/notfound

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = () => {
+export default class Header extends React.PureComponent {
+    render() {
+        const { context } = this.props;
+        let authUser = null;
+        if (context) {
+            authUser = context.authenticatedUser;
+        }
+
     return (
-        <div>
-            
+        <div className= "header">
+          <div className= "bounds">
+            <h1 className= "header--logo">Courses</h1>
+            <nav>
+              {authUser ? (
+                <React.Fragment>
+                    <span>Welcome, {authUser.name}!</span>
+                    <Link to="/sinout">Sign Out</Link>
+                </React.Fragment>  
+              ) : (
+                <React.Fragment>
+                    <Link className="signup" to="/signup">Sign Up</Link>
+                    <Link className="signin" to="/signin">Sign In</Link>
+                </React.Fragment>
+              )}
+            </nav>
+          </div>
         </div>
     );
+  }
 };
-
-export default Header;
