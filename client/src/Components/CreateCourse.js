783135fb1642319d91014f0ee58e34f55d3f5094
Simone Beaver <simone.beaver@verizon.com>
2019-09-26 09:13:17 -0500

    Update CreateCourse.js

--- client/src/Components/CreateCourse.js
+++ client/src/Components/CreateCourse.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 import CourseForm from './CourseForm';
 
 //Stateful class component
+
 export default class CreateCourse extends Component {
-    state = {     // The initial state of the course
+    state = {             
         title: '',
         description: '',
         estimatedTime: '',
@@ -20,54 +21,59 @@ export default class CreateCourse extends Component {
             materialsNeeded,
             errors,
         } = this.state;
-        // const { context } = this.props;
-        const authUser = {};
-        return (
-            <CourseForm
-                cancel={this.cancel}
-                errors={errors}
-                submit={this.submit}
-                submitButtonText="Create Course"
-                elements={() => (
-                    <React.Fragment>
-                        <div className="grid-66">
-                            <div className="course--header">
-                                <h4 className="course--label">Course</h4>
-                                <div>
-                                    <input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..." value={title} onChange={this.change} /></div>
-                                <p>By {authUser.name} </p>
-                            </div>
-                            <div className="course--description">
-                                <div><textarea id="description" name="description" className="" placeholder="Course description..." onChange={this.change} value={description} ></textarea>
+
+        const { context } = this.props;
+        const authUser = context.authenticatedUser;
+
+        return (       
+                 <CourseForm
+                    cancel={this.cancel}
+                    errors={errors}
+                    submit={this.submit}
+                    submitButtonText="Create Course"
+                    elements={() => (
+                        <React.Fragment>
+                            <div className="grid-66">
+                                <div className="course--header">
+                                    <h4 className="course--label">Course</h4>
+                                    <div><input id="title" name="title" type="text" className="input-title course--title--input" placeholder="Course title..."
+                                        value={title} onChange={this.change} /></div>
+                                        <p>By {authUser.name} </p>
+                                </div>
+                                <div className="course--description">
+                                    <div><textarea id="description" name="description" className="" placeholder="Course description..." onChange={this.change} value={description} ></textarea></div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="grid-25 grid-right">
-                            <div className="course--stats">
-                                <ul className="course--stats--list">
-                                    <li className="course--stats--list--item">
-                                        <h4>Estimated Time</h4>
-                                        <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input" placeholder="Hours" value={estimatedTime} onChange={this.change} /></div>
-                                    </li>
-                                    <li className="course--stats--list--item">
-                                        <h4>Materials Needed</h4>
-                                        <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." onChange={this.change} value={materialsNeeded} ></textarea></div>
-                                    </li>
-                                </ul>
+                            <div className="grid-25 grid-right">
+                                <div className="course--stats">
+                                    <ul className="course--stats--list">
+                                        <li className="course--stats--list--item">
+                                            <h4>Estimated Time</h4>
+                                            <div><input id="estimatedTime" name="estimatedTime" type="text" className="course--time--input"
+                                                placeholder="Hours" value={estimatedTime} onChange={this.change} /></div>
+                                        </li>
+                                        <li className="course--stats--list--item">
+                                            <h4>Materials Needed</h4>
+                                            <div><textarea id="materialsNeeded" name="materialsNeeded" className="" placeholder="List materials..." onChange={this.change} value={materialsNeeded} ></textarea></div>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
-                        </div>
-                    </React.Fragment>
-                )} />
+                                
+                         </React.Fragment>
+                    )} />
+                   
         );
-    };
-    
+    }
+
     change = (event) => {
         const name = event.target.name;
         const value = event.target.value;
+
         this.setState(() => {
             return {
                 [name]: value
-            }
+            };
         });
     }
 
@@ -75,32 +81,32 @@ export default class CreateCourse extends Component {
         const { context } = this.props;
         const {
             title,
-
             description,
             estimatedTime,
             materialsNeeded,
         } = this.state;
 
+            
         const course = {
             title,
             description,
             estimatedTime,
             materialsNeeded,
         };
-
-        //Authorize user and validate errors
+                 //Auth user form context & validate errors
         const authUser = context.authenticatedUser;
         if (authUser == null) {
-            this.setState({ errors: [{ message: "You have to be logged in to create a course" }] });
+            this.setState({ errors: [{message: "You have to be logged in to create a course"}] });
             return;
         }
+
         context.data.createCourse(course, authUser.username, authUser.password)
             .then(errors => {
                 if (errors.length) {
                     this.setState({ errors: errors });
                 } else {
-                    this.setState({ errors: [] });
-                    this.props.history.push('/');
+                    this.setState({ errors: [] });   
+                    this.props.history.push('/');                      
                 }
             })
             .catch((err) => {
@@ -108,6 +114,7 @@ export default class CreateCourse extends Component {
                 this.props.history.push('/error');
             });
     }
+
     cancel = () => {
         this.props.history.push('/');
     }
