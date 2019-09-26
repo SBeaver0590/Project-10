845e0888388a26d76d63fdc97fef6de8082227f5
Simone Beaver <simone.beaver@verizon.com>
2019-09-26 08:52:00 -0500

    Update UserSignIn.js

--- client/src/Components/UserSignIn.js
+++ client/src/Components/UserSignIn.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import CourseForm from './CourseForm';
+import Form from './Form';
 
 export default class UserSignIn extends Component {
   state = {
-    username: '',
+    emailAddress: '',
     password: '',
     errors: [],
   }
 
   render() {
     const {
-      username,
+      emailAddress,
       password,
       errors,
     } = this.state;
@@ -20,27 +20,27 @@ export default class UserSignIn extends Component {
       <div className="bounds">
         <div className="grid-33 centered signin">
           <h1>Sign In</h1>
-          <CourseForm
+          <Form 
             cancel={this.cancel}
             errors={errors}
             submit={this.submit}
             submitButtonText="Sign In"
             elements={() => (
               <React.Fragment>
-                <input
-                  id="username"
-                  name="username"
+                <input 
+                  id="emailAddress" 
+                  name="emailAddress" 
                   type="text"
-                  value={username}
-                  onChange={this.change}
-                  placeholder="User Name" />
-                <input
-                  id="password"
+                  value={emailAddress} 
+                  onChange={this.change} 
+                  placeholder="Email Address" />
+                <input 
+                  id="password" 
                   name="password"
                   type="password"
-                  value={password}
-                  onChange={this.change}
-                  placeholder="Password" />
+                  value={password} 
+                  onChange={this.change} 
+                  placeholder="Password" />                
               </React.Fragment>
             )} />
           <p>
@@ -50,32 +50,27 @@ export default class UserSignIn extends Component {
       </div>
     );
   }
-
-
   change = (event) => {
     const name = event.target.name;
     const value = event.target.value;
-
     this.setState(() => {
       return {
         [name]: value
       };
     });
   }
-
-  submit = () => {
+  submit = () => {           
     const { context } = this.props;
-    const { from } = this.props.location.state || { from: { pathname: '/authenticated' } };
-    const { username, password } = this.state;
-
-    context.actions.signIn(username, password)
+    
+    const { emailAddress, password } = this.state;
+    context.actions.signIn(emailAddress, password)  
       .then((user) => {
         if (user === null) {
           this.setState(() => {
-            return { errors: ['Sign-in was unsuccessful'] };
+            return { errors: [ 'Sign-in was unsuccessful' ] };
           });
         } else {
-          this.props.history.push(from);
+          this.props.history.goBack();
         }
       })
       .catch((error) => {
@@ -83,10 +78,10 @@ export default class UserSignIn extends Component {
         this.props.history.push('/error');
       });
   }
-
   cancel = () => {
     this.props.history.push('/');
   }
 }
+
 
 
